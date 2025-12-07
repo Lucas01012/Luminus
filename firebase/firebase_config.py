abb4cb8ab917a12d5ac945ccad37bfa176936fbc
Lucas Oliveira <lucas.silva600@fatec.sp.gov.br>
2025-12-07 09:08:43 -0300

    fix: ajustes na env

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -1,7 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, auth
+import os
 
-cred = credentials.Certificate("credentials/firebase-service-account.json")
+credential_file = os.getenv("FIREBASE_CREDENTIALS", "credentials/firebase-service-account.json")
+cred = credentials.Certificate(credential_file)
 firebase_admin.initialize_app(cred)
 
 auth_client = auth
